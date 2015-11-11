33e4f341ced0b853d9c1baa2746ee7bfdf16da66
528528lololo <ska4ko.col@yandex.ru>
2015-11-11 22:52:21 +0600

    Create Form1.h

---
+++ Form1.h
@@ -0,0 +1,429 @@
+#pragma once
+
+/*
+        ____                                  __    _         ___          __  _ ____            ______  _           __            ____   ___
+	   / __ )__  __   ____  ____  ____  _____/ /_  (_)____   /   |  ____  / /_(_) __ )____ _____/ / __ \(_)  _____  / /  _   __   / __ \ <  /
+	  / __  / / / /  / __ \/ __ \/ __ \/ ___/ __ \/ / ___/  / /| | / __ \/ __/ / __  / __ `/ __  / /_/ / / |/_/ _ \/ /  | | / /  / / / / / / 
+	 / /_/ / /_/ /  / /_/ / /_/ / / / / /__/ / / / / /__   / ___ |/ / / / /_/ / /_/ / /_/ / /_/ / ____/ />  </  __/ /   | |/ /  / /_/ / / /  
+	/_____/\__, /  / .___/\____/_/ /_/\___/_/ /_/_/\___/  /_/  |_/_/ /_/\__/_/_____/\__,_/\__,_/_/   /_/_/|_|\___/_/    |___/   \____(_)_/   
+	 /____/  __/     __                 ______        ______        ___   ____ _________                                                
+	  /   |  / /___  / /_  ____ _       <  <  /       <  <  /       |__ \ / __ <  / ____/                                                
+	 / /| | / / __ \/ __ \/ __ `/       / // /        / // /        __/ // / / / /___ \                                                  
+	/ ___ |/ / /_/ / / / / /_/ /       / // /  _     / // /  _     / __// /_/ / /___/ /                                                  
+   /_/  |_/_/ .___/_/ /_/\__,_/       /_//_/  (_)   /_//_/  (_)   /____/\____/_/_____/                                                   
+	        /_/                                                                                                                           
+
+	*/
+namespace AntiBadPixels {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Сводка для Form1
+	/// </summary>
+	public ref class Form1 : public System::Windows::Forms::Form
+	{
+	public:
+		Form1(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		~Form1()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Button^  button1;
+	protected: 
+	private: System::Windows::Forms::CheckBox^  checkBox1;
+
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::Timer^  timer1;
+	private: System::Windows::Forms::TextBox^  textBox1;
+	private: System::Windows::Forms::GroupBox^  groupBox1;
+	private: System::Windows::Forms::PictureBox^  pictureBox1;
+	private: System::Windows::Forms::Panel^  panel1;
+	private: System::Windows::Forms::Label^  label3;
+	private: System::Windows::Forms::Button^  button2;
+	public: System::Windows::Forms::TextBox^  textBox2;
+	private: System::Windows::Forms::PictureBox^  pictureBox2;
+	private: System::Windows::Forms::CheckBox^  checkBox2;
+	private: System::Windows::Forms::Label^  label4;
+	private: System::Windows::Forms::Timer^  timer2;
+	private: System::Windows::Forms::Label^  label5;
+	private: System::Windows::Forms::Label^  label6;
+	private: System::Windows::Forms::Timer^  timer3;
+	private: System::Windows::Forms::PictureBox^  pictureBox3;
+
+	private: System::ComponentModel::IContainer^  components;
+
+	private:
+		/// <summary>
+		/// Требуется переменная конструктора.
+		/// </summary>
+
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Обязательный метод для поддержки конструктора - не изменяйте
+		/// содержимое данного метода при помощи редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->components = (gcnew System::ComponentModel::Container());
+			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(Form1::typeid));
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->checkBox1 = (gcnew System::Windows::Forms::CheckBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->timer1 = (gcnew System::Windows::Forms::Timer(this->components));
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->pictureBox2 = (gcnew System::Windows::Forms::PictureBox());
+			this->checkBox2 = (gcnew System::Windows::Forms::CheckBox());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->timer2 = (gcnew System::Windows::Forms::Timer(this->components));
+			this->timer3 = (gcnew System::Windows::Forms::Timer(this->components));
+			this->pictureBox3 = (gcnew System::Windows::Forms::PictureBox());
+			this->groupBox1->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->pictureBox2))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->pictureBox1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->pictureBox3))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Bottom | System::Windows::Forms::AnchorStyles::Left));
+			this->button1->Location = System::Drawing::Point(24, 206);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(127, 35);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Start > ";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &Form1::button1_Click);
+			// 
+			// checkBox1
+			// 
+			this->checkBox1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Bottom | System::Windows::Forms::AnchorStyles::Left));
+			this->checkBox1->AutoSize = true;
+			this->checkBox1->Location = System::Drawing::Point(25, 177);
+			this->checkBox1->Name = L"checkBox1";
+			this->checkBox1->Size = System::Drawing::Size(208, 17);
+			this->checkBox1->TabIndex = 1;
+			this->checkBox1->Text = L"Enhanced mode (Усиленный режим)";
+			this->checkBox1->UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			this->label1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Bottom | System::Windows::Forms::AnchorStyles::Left));
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(22, 150);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(106, 13);
+			this->label1->TabIndex = 3;
+			this->label1->Text = L"Speed color change:";
+			// 
+			// label2
+			// 
+			this->label2->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Bottom | System::Windows::Forms::AnchorStyles::Left));
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(33, 309);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(0, 13);
+			this->label2->TabIndex = 4;
+			// 
+			// timer1
+			// 
+			this->timer1->Tick += gcnew System::EventHandler(this, &Form1::timer1_Tick);
+			// 
+			// textBox1
+			// 
+			this->textBox1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Bottom | System::Windows::Forms::AnchorStyles::Left));
+			this->textBox1->Location = System::Drawing::Point(131, 151);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(100, 20);
+			this->textBox1->TabIndex = 5;
+			this->textBox1->Text = L"5";
+			this->textBox1->TextChanged += gcnew System::EventHandler(this, &Form1::textBox1_TextChanged);
+			// 
+			// groupBox1
+			// 
+			this->groupBox1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Bottom | System::Windows::Forms::AnchorStyles::Left));
+			this->groupBox1->BackColor = System::Drawing::Color::Transparent;
+			this->groupBox1->Controls->Add(this->label6);
+			this->groupBox1->Controls->Add(this->label5);
+			this->groupBox1->Controls->Add(this->pictureBox2);
+			this->groupBox1->Controls->Add(this->checkBox2);
+			this->groupBox1->Controls->Add(this->label4);
+			this->groupBox1->Controls->Add(this->label3);
+			this->groupBox1->Controls->Add(this->button2);
+			this->groupBox1->Controls->Add(this->textBox2);
+			this->groupBox1->Controls->Add(this->pictureBox1);
+			this->groupBox1->Location = System::Drawing::Point(12, 267);
+			this->groupBox1->Name = L"groupBox1";
+			this->groupBox1->Size = System::Drawing::Size(282, 99);
+			this->groupBox1->TabIndex = 6;
+			this->groupBox1->TabStop = false;
+			this->groupBox1->Text = L"Timer";
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->Cursor = System::Windows::Forms::Cursors::Cross;
+			this->label6->Font = (gcnew System::Drawing::Font(L"Lucida Bright", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point, 
+				static_cast<System::Byte>(0)));
+			this->label6->Location = System::Drawing::Point(136, 76);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(0, 24);
+			this->label6->TabIndex = 8;
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Cursor = System::Windows::Forms::Cursors::Cross;
+			this->label5->Font = (gcnew System::Drawing::Font(L"Lucida Bright", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point, 
+				static_cast<System::Byte>(0)));
+			this->label5->Location = System::Drawing::Point(102, 76);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(17, 24);
+			this->label5->TabIndex = 7;
+			this->label5->Text = L" ";
+			// 
+			// pictureBox2
+			// 
+			this->pictureBox2->Image = (cli::safe_cast<System::Drawing::Image^  >(resources->GetObject(L"pictureBox2.Image")));
+			this->pictureBox2->Location = System::Drawing::Point(109, 48);
+			this->pictureBox2->Name = L"pictureBox2";
+			this->pictureBox2->Size = System::Drawing::Size(24, 21);
+			this->pictureBox2->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
+			this->pictureBox2->TabIndex = 6;
+			this->pictureBox2->TabStop = false;
+			// 
+			// checkBox2
+			// 
+			this->checkBox2->AutoSize = true;
+			this->checkBox2->Location = System::Drawing::Point(96, 52);
+			this->checkBox2->Name = L"checkBox2";
+			this->checkBox2->Size = System::Drawing::Size(186, 17);
+			this->checkBox2->TabIndex = 5;
+			this->checkBox2->Text = L"      Turn off your PC after the task";
+			this->checkBox2->UseVisualStyleBackColor = true;
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(201, 23);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(44, 13);
+			this->label4->TabIndex = 4;
+			this->label4->Text = L"Minutes";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(9, 22);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(79, 13);
+			this->label3->TabIndex = 3;
+			this->label3->Text = L"Stop following: ";
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(0, 48);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(89, 23);
+			this->button2->TabIndex = 2;
+			this->button2->Text = L"button2";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &Form1::button2_Click);
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(94, 20);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(100, 20);
+			this->textBox2->TabIndex = 1;
+			this->textBox2->Text = L"15";
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^  >(resources->GetObject(L"pictureBox1.Image")));
+			this->pictureBox1->Location = System::Drawing::Point(35, 0);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(21, 20);
+			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
+			this->pictureBox1->TabIndex = 0;
+			this->pictureBox1->TabStop = false;
+			// 
+			// panel1
+			// 
+			this->panel1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom) 
+				| System::Windows::Forms::AnchorStyles::Left) 
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->panel1->BackColor = System::Drawing::Color::Green;
+			this->panel1->Location = System::Drawing::Point(2, 4);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(292, 141);
+			this->panel1->TabIndex = 7;
+			// 
+			// timer2
+			// 
+			this->timer2->Tick += gcnew System::EventHandler(this, &Form1::timer2_Tick);
+			// 
+			// timer3
+			// 
+			this->timer3->Tick += gcnew System::EventHandler(this, &Form1::timer3_Tick);
+			// 
+			// pictureBox3
+			// 
+			this->pictureBox3->Image = (cli::safe_cast<System::Drawing::Image^  >(resources->GetObject(L"pictureBox3.Image")));
+			this->pictureBox3->Location = System::Drawing::Point(167, 200);
+			this->pictureBox3->Name = L"pictureBox3";
+			this->pictureBox3->Size = System::Drawing::Size(39, 41);
+			this->pictureBox3->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
+			this->pictureBox3->TabIndex = 8;
+			this->pictureBox3->TabStop = false;
+			this->pictureBox3->Click += gcnew System::EventHandler(this, &Form1::pictureBox3_Click);
+			// 
+			// Form1
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::SystemColors::ControlLight;
+			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^  >(resources->GetObject(L"$this.BackgroundImage")));
+			this->ClientSize = System::Drawing::Size(298, 371);
+			this->Controls->Add(this->pictureBox3);
+			this->Controls->Add(this->panel1);
+			this->Controls->Add(this->groupBox1);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->checkBox1);
+			this->Controls->Add(this->button1);
+			this->ForeColor = System::Drawing::SystemColors::ControlText;
+			this->Name = L"Form1";
+			this->Text = L"AntiBadPixaels v 0.1 Alpha By ponchic";
+			this->Load += gcnew System::EventHandler(this, &Form1::Form1_Load);
+			this->Click += gcnew System::EventHandler(this, &Form1::Form1_Click);
+			this->groupBox1->ResumeLayout(false);
+			this->groupBox1->PerformLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->pictureBox2))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->pictureBox1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->pictureBox3))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+		
+
+	private: System::Void Form1_Load(System::Object^  sender, System::EventArgs^  e) {
+				 button2->Text = "Start > ";
+			 }
+	private: System::Void Form1_Click(System::Object^  sender, System::EventArgs^  e) {
+			 }
+private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+			 if(button1->Text == "Start > ") { 
+				 button1->Text = "Stop || ";
+				
+				 
+				 int intrvl = Convert::ToDouble( textBox1->Text );
+                   (checkBox1->Checked ? intrvl /= 2 : intrvl = intrvl );
+				 timer1->Interval = intrvl;
+				 timer1->Start();
+				
+			 }
+			 else {
+				 button1->Text = "Start > ";
+				 timer1->Stop();
+			 }
+		 }
+
+
+
+
+
+private: System::Void timer1_Tick(System::Object^  sender, System::EventArgs^  e) {
+			 System::Random^ Randomizer = gcnew System::Random();  //Создание генератора случайных чисел
+			 int R = Randomizer->Next(0, 255);  //Это будет красный компонент
+			 int G = Randomizer->Next(0, 255);  //Это будет зелёный компонент
+			 int B = Randomizer->Next(0, 255);  //Это будет синий компонент
+			 //myColor - цвет, образованный смешением красного, зелёного и синего компонентов.
+			 System::Drawing::Color myColor = System::Drawing::Color::FromArgb(R, G, B);
+			 this->panel1->BackColor = myColor;
+
+		 }
+private: System::Void textBox1_TextChanged(System::Object^  sender, System::EventArgs^  e) {
+		 }
+private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) {
+			 if(button2->Text == "Start > ") { 
+				 button2->Text = "Stop || ";
+
+
+				 int intrvl = Convert::ToDouble( textBox2->Text );
+
+				 int res = intrvl * 60 * 1000; 
+				 timer2->Interval = res; 
+				 timer2->Start();
+
+				 timer3->Interval = 1000;
+                 timer3->Start();
+				
+			 }
+			 else {
+				 button2->Text = "Start > ";
+				 timer2->Stop();
+			 }
+		 }
+private: System::Void timer2_Tick(System::Object^  sender, System::EventArgs^  e) {
+			
+			 if (checkBox2->Checked)
+			 {
+				 Diagnostics::Process::Start("ShutDown", "/s");
+				 
+			 }
+			 timer1->Stop();
+			 timer2->Stop();
+		}
+		 
+				 //static int i = Convert::ToDouble( textBox2->Text ) * 60;
+
+
+private: System::Void timer3_Tick(System::Object^  sender, System::EventArgs^  e) {
+			 /* label6->Text = Convert::ToString(i);
+			 i--;
+			 if (i <= 0)
+			 {
+				 timer3->Stop();
+			 }
+			 */
+		 }
+private: System::Void pictureBox3_Click(System::Object^  sender, System::EventArgs^  e) {
+			 MessageBox::Show("Place the colored area so that the stuck pixels were \n on it and select the speed of the \n color change, you can set the timer . \n If you tick then your computer turns off \n after the completion of the correction\n pixels . \n There nshli requests or bug?\n Tell us about it in the mail 528528lololo@gmail.com \n \n RU: \n Разместите цветную область так, чтобы застрявшие пиксели оказались на ней и выберите скорость смены цветов, Можете установить таймер. Если поставите галочку то ваш компьютер выключится после завершения работы по исправлению пикселей. Есть пожелания или ншли баг? Сообщите об этом нам на почту 528528lololo@gmail.com", "Help");
+		 }
+};
+}
+
